ebf87ba799bc1a5d183c864a7bf1c3c02151eb5b
Gilpop8663 <wolfye@naver.com>
2022-01-28 04:08:04 +0900

    Add - 어드민 페이지 Layout 디자인 추가

---
+++ src/layout/CommonLayout.js
@@ -0,0 +1,198 @@
+import styled from 'styled-components';
+import Tab from './Tab';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  height: 55px;
+  background-color: #060606;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding-left: 20px;
+`;
+
+const Logo = styled.img`
+  background-image: center center;
+  background-size: cover;
+`;
+
+const HeaderTitle = styled.span`
+  color: white;
+  font-size: 16px;
+  font-weight: 400;
+  margin-left: 20px;
+`;
+
+const RegisterProduct = styled.div`
+  padding: 20px 35px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+  background-color: white;
+  border-bottom: 2px solid ${({ theme }) => theme.subBgColor};
+`;
+
+const RegisterTitle = styled.span`
+  font-size: 16px;
+  margin-left: 10px;
+  font-weight: 600;
+  color: black;
+`;
+
+const RegisterGrayTitle = styled(RegisterTitle)`
+  font-size: 14px;
+  color: #6c6c6c;
+  font-weight: 500;
+`;
+
+const Overview = styled.div`
+  padding: 25px;
+  display: flex;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.bgColor};
+`;
+
+const ContentGrid = styled.div`
+  background-color: ${({ theme }) => theme.subBgColor};
+  width: 100%;
+  display: grid;
+  grid-template-rows: 50px 40px 1fr;
+  border: 2px solid rgba(0, 0, 0, 0.5);
+  gap: 1px;
+`;
+
+const Menu = styled.div`
+  height: 150vh;
+  width: 100%;
+  background-color: tomato;
+`;
+
+const Content = styled.div`
+  display: flex;
+  background-color: white;
+  flex-direction: column;
+  justify-content: flex-start;
+  height: 100%;
+  width: 100%;
+`;
+
+const Items = styled.ul`
+  background-color: white;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 17px;
+`;
+
+const Item = styled.li`
+  display: flex;
+  background-color: ${({ theme }) => theme.bgColor};
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding-left: 17px;
+  cursor: pointer;
+  font-weight: 600;
+  &:nth-child(3) {
+    background-color: #d6d5e2;
+    color: ${({ theme }) => theme.pointColor};
+  }
+`;
+
+const ThreeGrid = styled.ul`
+  display: grid;
+  grid-template-rows: repeat(3, 40px);
+  background-color: ${({ theme }) => theme.subBgColor};
+`;
+
+const Row = styled.div`
+  display: grid;
+  gap: 1px;
+  grid-template-columns: 1fr 4.75fr;
+`;
+
+const MainHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  height: 100%;
+  padding: 0px 17px;
+`;
+
+const MainHeaderSpan = styled.span`
+  font-weight: 600;
+`;
+const MainHeaderButton = styled.div`
+  padding: 10px 17px;
+  background-color: ${({ theme }) => theme.pointColor};
+  border-radius: 5px;
+  color: ${({ theme }) => theme.bgColor};
+  cursor: pointer;
+`;
+
+const BigItems = styled.div`
+  display: grid;
+  gap: 1px;
+  grid-template-rows: repeat(3, 40px) 120px repeat(6, 40px) 1fr;
+`;
+
+export function CommonLayout() {
+  return (
+    <Container>
+      <Header>
+        <Logo src="https://user-images.githubusercontent.com/80146176/151404279-fde572a0-77ba-40a8-95d2-489113e1f4e0.png" />
+        <HeaderTitle>ADMIN</HeaderTitle>
+      </Header>
+      <RegisterProduct>
+        <RegisterTitle>상품 등록</RegisterTitle>
+        <RegisterGrayTitle>ADM_PRODUCT_AD</RegisterGrayTitle>
+      </RegisterProduct>
+      <Overview>
+        <ContentGrid>
+          <Row>
+            <Items>
+              <Logo src="https://user-images.githubusercontent.com/80146176/151408338-468d2544-baf5-4b10-b987-a8e9c1856bd5.png"></Logo>
+            </Items>
+            <Items></Items>
+          </Row>
+          <Row>
+            <Tab title="기본 설정" />
+            <MainHeader>
+              <MainHeaderSpan>상품 등록</MainHeaderSpan>
+              <MainHeaderButton>저장하기</MainHeaderButton>
+            </MainHeader>
+          </Row>
+          <Row>
+            <BigItems>
+              <Tab title="회원" />
+              <Tab title="진열" />
+              <Tab title="상품" />
+              <ThreeGrid>
+                <Item>상품 리스트</Item>
+                <Item>상품 재고 관리</Item>
+                <Item>상품 등록</Item>
+              </ThreeGrid>
+              <Tab title="주문" />
+              <Tab title="배송" />
+              <Tab title="프로모션" />
+              <Tab title="혜택" />
+              <Tab title="고객 센터 관리" />
+              <Tab title="알림" />
+              <Items></Items>
+            </BigItems>
+            <Content>
+              <Menu />
+            </Content>
+          </Row>
+        </ContentGrid>
+      </Overview>
+    </Container>
+  );
+}
--- src/pages/admin/components/exposureNSalesPeriod/ExposureNSalesPeriod.js
+++ src/pages/admin/components/exposureNSalesPeriod/ExposureNSalesPeriod.js
@@ -5,14 +5,25 @@ import styled from 'styled-components';
 export default function ExposureNSalesPeriod() {
   const onChange = useCallback(() => console.log('onChange'));
   return (
-    <FormTextArea
-      attrs={{
-        name: '모야',
-        placeholder: '검색어를 입력해주세요',
-        onChange,
-        rows: 3,
-        cols: 3,
-      }}
-    />
+    <>
+      <FormTextArea
+        attrs={{
+          name: '모야',
+          placeholder: '검색어를 입력해주세요',
+          onChange,
+          rows: 1,
+          cols: 1,
+        }}
+      />
+      <FormTextArea
+        attrs={{
+          name: '김치전',
+          placeholder: '필터 태그를 입력하세여',
+          onChange,
+          rows: 1,
+          cols: 1,
+        }}
+      />
+    </>
   );
 }
