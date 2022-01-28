5537a5649393a52b2708d1c5b647c852cec4ff8b
limbs722 <limbs722@gmail.com>
2022-01-28 14:48:22 +0900

    Modify - 코드 정리 및 상품 옵션 수정

--- src/pages/admin/components/exposureNSalesPeriod/ExposureNSalesPeriod.js
+++ src/pages/admin/components/exposureNSalesPeriod/ExposureNSalesPeriod.js
@@ -1,30 +1,5 @@
-import { FormTextArea } from 'components';
-import { useCallback } from 'react';
 import styled from 'styled-components';
-import { FormCheckBox } from 'components';
 
 export default function ExposureNSalesPeriod() {
-  const onChange = useCallback(() => console.log('onChange'));
-  return (
-    <>
-      <FormTextArea
-        attrs={{
-          name: '모야',
-          placeholder: '검색어를 입력해주세요',
-          onChange,
-          rows: 1,
-          cols: 1,
-        }}
-      />
-      <FormTextArea
-        attrs={{
-          name: '김치전',
-          placeholder: '필터 태그를 입력하세여',
-          onChange,
-          rows: 1,
-          cols: 1,
-        }}
-      />
-    </>
-  );
+  return null;
 }
--- src/pages/admin/components/productOption/ProductOption.js
+++ src/pages/admin/components/productOption/ProductOption.js
@@ -1,5 +1,47 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const ProductOptionStyled = styled.div`
+  width: 80%;
+  background-color: inherit;
+  border-radius: 30px;
+  border: 2px solid gray;
+`;
+
+const TitleBar = styled.div`
+  /* display: inline-flex;
+  flex-direction: row;
+  justify-content: space-between; */
+`;
+
+const FormList = styled.div`
+  width: 800px;
+  height: 500px;
+  background-color: gray;
+`;
+
 export default function ProductOption() {
-  return null;
+  const [optionSetArr, SetOptionSetArr] = useState([]);
+
+  const onClick = (e, type) => {
+    switch (type) {
+      case 'addOption':
+        console.log('옵션추가');
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <ProductOptionStyled>
+      <TitleBar>
+        <h5>상품옵션</h5>
+        <button onClick={(e) => onClick(e, 'addOption')}>
+          + 옵션 세트 추가
+        </button>
+      </TitleBar>
+      <FormList>옵션세트를 추가하여 옵션을 구성해주세요</FormList>
+    </ProductOptionStyled>
+  );
 }
